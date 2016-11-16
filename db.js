976a49fe4759a9a9957d0d152c00abe1805d5c80
Giuseppe Tomasello <gtomasello90@gmail.com>
2016-11-17 04:26:47 +0800

    ready to deploy to aws

---
+++ db.js
@@ -0,0 +1,30 @@
+var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if(env === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        'dialect':'postgres'
+    });
+
+}else{
+
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect':'sqlite',
+        'storage': __dirname  + '/data/dev-todo-api.sqlite'
+    });
+}
+var db = {};
+db.word_test = sequelize.import(__dirname + '/models/word_test.js');
+db.worddb = sequelize.import(__dirname + '/models/worddb.js');
+db.wordbook = sequelize.import(__dirname + '/models/wordbook.js');
+db.wordzh = sequelize.import(__dirname + '/models/wordzh.js');
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.user = sequelize.import(__dirname + '/models/user.js');
+db.token = sequelize.import(__dirname + '/models/token.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.todo.belongsTo(db.user);
+db.user.hasMany(db.todo);
+
+module.exports =db;
